6764e979fbaec03d005921516b4220c01f166177
BERNtortik <berntortik@gmail.com>
2024-09-27 22:00:40 +0300

    Add files via upload

---
+++ program_3.py
@@ -0,0 +1,11 @@
+a1 = float(input("введите первый член прогрессии a1: ")) 
+d = float(input("введите шаг d: ")) 
+n = int(input("введите количество членов n: ")) 
+progression = [] 
+for n in range(n): 
+    gin = a1 + n * d 
+progression.append(gin)  
+print("первые", n, "членов арифметической прогрессии:") 
+for ginil in progression: 
+    print(ginil, end=" ")
+"README.md."
